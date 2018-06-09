1b36342e2c547da3ada9d4bc9f8561c893d352e7
Oleh <pryatko.oleg@gmail.com>
2018-06-09 10:47:06 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { PhoneFormComponent } from './phone-form/phone-form.component';
-import { ListComponent } from './list/list.component';
-import { MoreComponent } from './more/more.component';
+import {AppComponent} from './app.component';
+import {DataService} from "./data.service";
+import {PhoneFormComponent} from "./phone-form/phone-form.component";
+import {MoreComponent} from "./more/more.component";
+import {ListComponent} from "./list/list.component";
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PhoneFormComponent,
-    ListComponent,
-    MoreComponent
-  ],
-  imports: [
-    BrowserModule, FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PhoneFormComponent,
+        MoreComponent,
+        ListComponent
+    ],
+    imports: [
+        BrowserModule, FormsModule
+    ],
+    providers: [DataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -2,10 +2,10 @@ import {Component, EventEmitter, Input} from '@angular/core';
 import {Phone} from "../entities/phone";
 
 @Component({
-  selector: 'app-more',
-  templateUrl: './more.component.html',
-  styleUrls: ['./more.component.css']
+    selector: 'app-more',
+    templateUrl: './more.component.html',
+    styleUrls: ['./more.component.css']
 })
 export class MoreComponent {
-  @Input('active') active: Phone;
+    @Input('active') active: Phone;
 }
